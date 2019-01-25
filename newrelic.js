3474cb430b645affd7c2c65c16ff8f66694291df
David Ngo <ngo.david.huy@gmail.com>
2019-01-25 15:26:57 -0800

    Fixes newrelic bug

--- newrelic.js
+++ newrelic.js
@@ -5,7 +5,6 @@
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-
 const keys = require('./config').newrelic
 
 exports.config = {
